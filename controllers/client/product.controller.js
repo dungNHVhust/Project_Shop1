6980146f9255640f3b35d9548944b2130e70dfe9
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-08-15 16:34:36 +0700

    Làm trang danh sách sản phẩm bên Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,20 @@
-module.exports.product=(req,res)=>{
+const Product = require("../../models/product.model.js")
+
+module.exports.product=async (req,res)=>{
+    const products = await Product.find({
+        status:"active",
+        deleted: false
+    });
+
+    const newProducts = products.map(item =>{
+        item.priceNew=(item.price*(100-item.discountPercentage)/100).toFixed(0);
+        return item;
+    });
+
+    console.log(newProducts);
+
     res.render("client/pages/products/index.pug",{
-        pageTitle:"Trang sản phẩm"
+        pageTitle:"Trang sản phẩm",
+        products:newProducts 
     });
 }
