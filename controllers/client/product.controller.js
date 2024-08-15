3f1dfc0e961d61c989947c3c6fbfd7deaedcc8e7
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-08-15 16:55:48 +0700

    no cap

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,20 +1,23 @@
-const Product = require("../../models/product.model.js")
+const Product = require("../../models/product.model.js");
 
-module.exports.product=async (req,res)=>{
-    const products = await Product.find({
-        status:"active",
-        deleted: false
-    });
+module.exports.product = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false,
+  });
 
-    const newProducts = products.map(item =>{
-        item.priceNew=(item.price*(100-item.discountPercentage)/100).toFixed(0);
-        return item;
-    });
+  const newProducts = products.map((item) => {
+    item.priceNew = (
+      (item.price * (100 - item.discountPercentage)) /
+      100
+    ).toFixed(0);
+    return item;
+  });
 
-    console.log(newProducts);
+  console.log(newProducts);
 
-    res.render("client/pages/products/index.pug",{
-        pageTitle:"Trang sản phẩm",
-        products:newProducts 
-    });
-}
+  res.render("client/pages/products/index.pug", {
+    pageTitle: "Trang sản phẩm",
+    products: newProducts,
+  });
+};
--- models/product.model.js
+++ models/product.model.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 
 const product_schema = new mongoose.Schema({
-    title:String,
-    description:String,
-    price:Number,
-    discountPercentage:Number,
-    stock:Number,
-    status:String,
-    thumbnail:String,
-    position:Number,
-    deleted:Boolean
-
+  title: String,
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  stock: Number,
+  status: String,
+  thumbnail: String,
+  position: Number,
+  deleted: Boolean,
 });
-const Product = mongoose.model('Product',product_schema,'products');
+const Product = mongoose.model("Product", product_schema, "products");
 
 module.exports = Product;
