94fdc5ce99d54aaa89394c95e94a07b96fc2d618
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-08-16 14:43:40 +0700

    Làm giao diện danh sách sản phẩm ở trang Admin

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,9 +1,11 @@
 const dashboardRoutes = require("./dashboard.route.js");
+const productRoutes = require("./product.route.js");
 const systemConfix = require("../../config/system.js");
 
 module.exports= (app) =>{
     const PATH_ADMIN = systemConfix.prefixAdmin;
-    //Nhúng dashboard
+    //Nhúng dashboard,products
     app.use(PATH_ADMIN+ "/dashboard",dashboardRoutes);
+    app.use(PATH_ADMIN+ "/products",productRoutes);
             
 }
