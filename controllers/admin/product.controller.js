392c4f2afa7b76479d228ea4b32537c655938750
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-08-16 15:07:42 +0700

    Lấy data sản phẩm in ra giao diện ở trang Admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,12 @@
+const Product=require("../../models/product.model.js");
 // [GET] /admin/products
-module.exports.index = (req, res) => {
-  res.render("admin/pages/products/index.pug", {
+module.exports.index =async (req, res) => {
+    const product=await Product.find({
+        deleted: false
+    });
+    console.log(product);
+    res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang sản phẩm",
+    product:product
   });
 };
