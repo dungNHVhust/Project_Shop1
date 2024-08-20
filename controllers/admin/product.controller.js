9b6cb53bbd55e1c7f82e4ef1987529daefc5ba74
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-08-20 23:57:42 +0700

    Tính năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,12 +1,44 @@
 const Product=require("../../models/product.model.js");
 // [GET] /admin/products
 module.exports.index =async (req, res) => {
-    const product=await Product.find({
-        deleted: false
-    });
-    console.log(product);
+  let filterStatus = [
+    {
+      name:"Tất cả",
+      status:"",
+      class:""  
+    },
+    {
+      name:"Hoạt động",
+      status:"active",
+      class:""  
+    },
+    {
+      name: "Dừng hoạt động",
+      status: "inactive",
+      class: ""  
+    }
+  ];
+  if(req.query.status){
+    const index = filterStatus.findIndex(item => item.status == req.query.status);
+    filterStatus[index].class="active";
+  }else {
+    const index = filterStatus.findIndex(item => item.status == "");
+    filterStatus[index].class="active";
+  }
+
+    let find ={
+      deleted:false
+  };
+    if(req.query.status){
+      find.status = req.query.status
+    }
+    const product=await Product.find(find);
+    console.log(req.query.status);
     res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang sản phẩm",
-    product:product
+    product:product,
+    filterStatus:filterStatus
   });
 };
+
+
