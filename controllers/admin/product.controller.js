d6feecfb9fd37b04cbb3a67643ed179716d9bf68
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-08-22 22:10:46 +0700

    Tính năng tìm kiếm(BackEnd)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,44 +1,51 @@
-const Product=require("../../models/product.model.js");
+const Product = require("../../models/product.model.js");
 // [GET] /admin/products
-module.exports.index =async (req, res) => {
+module.exports.index = async (req, res) => {
   let filterStatus = [
     {
-      name:"Tất cả",
-      status:"",
-      class:""  
+      name: "Tất cả",
+      status: "",
+      class: "",
     },
     {
-      name:"Hoạt động",
-      status:"active",
-      class:""  
+      name: "Hoạt động",
+      status: "active",
+      class: "",
     },
     {
       name: "Dừng hoạt động",
       status: "inactive",
-      class: ""  
-    }
+      class: "",
+    },
   ];
-  if(req.query.status){
-    const index = filterStatus.findIndex(item => item.status == req.query.status);
-    filterStatus[index].class="active";
-  }else {
-    const index = filterStatus.findIndex(item => item.status == "");
-    filterStatus[index].class="active";
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status
+    );
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
   }
 
-    let find ={
-      deleted:false
+  let find = {
+    deleted: false
   };
-    if(req.query.status){
-      find.status = req.query.status
-    }
-    const product=await Product.find(find);
-    console.log(req.query.status);
-    res.render("admin/pages/products/index.pug", {
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  let keyword = "";
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
+  const product = await Product.find(find);
+  console.log(req.query.status);
+  res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang sản phẩm",
-    product:product,
-    filterStatus:filterStatus
+    product: product,
+    filterStatus: filterStatus,
+    keyword: keyword,
   });
 };
-
-
