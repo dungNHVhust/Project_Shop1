e0b095c4cbbc45de71d0f81b28ea6936933ff4b9
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-08-22 23:14:54 +0700

    Tối ưu phần bộ lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,32 +1,10 @@
 const Product = require("../../models/product.model.js");
+const filterStatusHelper = require("../../helpers/filterStatus.js");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  let filterStatus = [
-    {
-      name: "Tất cả",
-      status: "",
-      class: "",
-    },
-    {
-      name: "Hoạt động",
-      status: "active",
-      class: "",
-    },
-    {
-      name: "Dừng hoạt động",
-      status: "inactive",
-      class: "",
-    },
-  ];
-  if (req.query.status) {
-    const index = filterStatus.findIndex(
-      (item) => item.status == req.query.status
-    );
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex((item) => item.status == "");
-    filterStatus[index].class = "active";
-  }
+  //Đoạn bộ lọc
+  const filterStatus = filterStatusHelper(req.query);
 
   let find = {
     deleted: false
