d6b62ee1041f1592f3a5fc4654349911f253ddb7
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-08-29 21:20:30 +0700

    Tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -18,12 +18,28 @@ module.exports.index = async (req, res) => {
   if (objectSearch.regex){
     find.title = objectSearch.regex;
   }
-  const product = await Product.find(find);
+  // Pagiantion
+  let objectPagination = {
+    currentPage: 1,
+    limitItem : 4
+  };
+  if(req.query.page){
+    objectPagination.currentPage=parseInt(req.query.page);
+  }
+
+  objectPagination.skip = (objectPagination.currentPage - 1)*objectPagination.limitItem;
+  const countProducts = await  Product.countDocuments(find);
+  const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
+  objectPagination.totalPage = totalPage;
+  //End Pagination
+
+  const product = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
   console.log(req.query.status);
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Trang sản phẩm",
     product: product,
     filterStatus: filterStatus,
-    keyword: objectSearch.keyword
+    keyword: objectSearch.keyword,
+    pagination: objectPagination
   });
 };
