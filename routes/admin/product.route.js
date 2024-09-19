ff85a52be10434b42226bfc6e2954cf8858a66f7
Dung.nhv <144256909+dungNHVhust@users.noreply.github.com>
2024-09-19 21:54:20 +0700

    Đẩy file tĩnh lên Cloud

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -2,26 +2,30 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../../controllers/admin/product.controller.js");
 const validates = require("../../validates/admin/product.validate.js");
-const storageMulter = require("../../helpers/storageMulter.js");
-const multer = require('multer'); //Import multer
+// const storageMulter = require("../../helpers/storageMulter.js");
+const multer = require("multer"); //Import multer
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middlewares.js");
 
-const upload = multer({storage:storageMulter()});
+const upload = multer();
 
 router.get("/", controller.index);
 router.patch("/change-status/:status/:id", controller.changeStatus);
-router.patch("/change-multi/",controller.changeMulti)
+router.patch("/change-multi/", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
-router.post("/create"
-    ,upload.single('thumbnail'),
-    validates.createPost,
-    controller.createPost
+router.post(
+  "/create",
+  upload.single("thumbnail"),
+  uploadCloud.uploadCld,
+  validates.createPost,
+  controller.createPost
 );
 router.get("/edit/:id", controller.edit);
-router.patch("/edit/:id",
-    upload.single("thumbnail"),
-    validates.editProduct,
-    controller.editProduct
+router.patch(
+  "/edit/:id",
+  upload.single("thumbnail"),
+  validates.editProduct,
+  controller.editProduct
 );
 router.get("/detail/:id", controller.detail);
 
